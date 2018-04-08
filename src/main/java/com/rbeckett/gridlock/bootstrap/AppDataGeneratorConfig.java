51525ca6d86796e3dfb3be60980fc7b07ba6cd7a
Ryan Beckett <beckett.ryan@gmail.com>
2018-04-08 14:50:38 -0400

    - (1) Decreased amount of mock data generated.

--- src/main/java/com/rbeckett/gridlock/bootstrap/AppDataGeneratorConfig.java
+++ src/main/java/com/rbeckett/gridlock/bootstrap/AppDataGeneratorConfig.java
@@ -26,18 +26,18 @@ public class AppDataGeneratorConfig {
     public static final int NUM_SERVICE_CONTRACTS = 10;
     public static final int NUM_SITES = 10;
     public static final int NUM_ROOMS = NUM_SITES * 2;
-    public static final int NUM_RACKS = 1500;
+    public static final int NUM_RACKS = 150;
     public static final int NUM_GRID_LOCATIONS = (int) (Math.pow(GridUtils.MAX_GRID_INDEX, 2)) / 3;
-    public static final int NUM_DESKTOP_DEVICES = 250;
-    public static final int NUM_MAIN_FRAMES = 500;
-    public static final int NUM_NETWORK_DEVICES = 5000;
-    public static final int NUM_PATCH_PANELS = 2500;
-    public static final int NUM_SERVER_DEVICES = 10000;
-    public static final int NUM_STORAGE_DEVICES = 5000;
-    public static final int NUM_STORAGE_FRAMES = 500;
-    public static final int NUM_NETWORK_CONFIGURATIONS = NUM_DESKTOP_DEVICES + NUM_MAIN_FRAMES + NUM_NETWORK_DEVICES
-            + NUM_SERVER_DEVICES + NUM_STORAGE_DEVICES + NUM_STORAGE_FRAMES;
-    public static final int NUM_HARDWARE_CONFIGURATIONS = NUM_NETWORK_CONFIGURATIONS;
-    public static final int NUM_OS_CONFIGURATIONS = NUM_NETWORK_CONFIGURATIONS;
-    public static final int NUM_NETWORK_CONNECTIONS = NUM_NETWORK_CONFIGURATIONS * 3;
+    public static final int NUM_DESKTOP_DEVICES = 25;
+    public static final int NUM_MAIN_FRAMES = 50;
+    public static final int NUM_NETWORK_DEVICES = 500;
+    public static final int NUM_PATCH_PANELS = 250;
+    public static final int NUM_SERVER_DEVICES = 1000;
+    public static final int NUM_STORAGE_DEVICES = 500;
+    public static final int NUM_STORAGE_FRAMES = 50;
+    public static final int NUM_NETWORK_CONFIGURATIONS = -1;//NUM_DESKTOP_DEVICES + NUM_MAIN_FRAMES + NUM_NETWORK_DEVICES
+    //+ NUM_SERVER_DEVICES + NUM_STORAGE_DEVICES + NUM_STORAGE_FRAMES;
+    public static final int NUM_HARDWARE_CONFIGURATIONS = -1;//NUM_NETWORK_CONFIGURATIONS;
+    public static final int NUM_OS_CONFIGURATIONS = -1;//NUM_NETWORK_CONFIGURATIONS;
+    public static final int NUM_NETWORK_CONNECTIONS = -1;//NUM_NETWORK_CONFIGURATIONS * 3;
 }
