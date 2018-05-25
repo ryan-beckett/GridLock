6239c0abb15c14714327e38ea115ec8c6c758131
Ryan Beckett <beckett.ryan@gmail.com>
2018-05-25 08:25:36 -0400

    - Added IMG URLs for Server Device Bootstrap class

--- src/main/java/com/rbeckett/gridlock/bootstrap/ServerDeviceGenerator.java
+++ src/main/java/com/rbeckett/gridlock/bootstrap/ServerDeviceGenerator.java
@@ -18,8 +18,16 @@ public class ServerDeviceGenerator extends RackableDeviceGenerator implements Ge
 
     private static final DataFactory dataFactory = new DataFactory();
     private static final String[] SERVER_DEVICES = {"SuperMicro SYS-5015A-EHF-D525",
-            "Supermicro AS-2022G", "Asus RS100-E9-P12", "PowerEdge T330", "Dell NetShelter",
-            "HP GridEdge 4550", "HP ComputerPower 3-Series"};
+            "Supermicro AS-2022G", "Asus RS100-E9-P12", "PowerEdge T330", "Dell Server r730",
+            "HP GridEdge 4550", "Cisco Blade I930"};
+    private static final String[] SERVER_DEVICES_IMG_URLS = {
+            "https://www.supermicro.com/products/system/1U/5015/SYS-5015A-EHF-D525.cfm",
+            "https://www.supermicro.com/aplus/system/2u/2022/as-2022g-urf4_.cfm",
+            "http://www.taknet.com.my/products/asus/rs100-e9-p12/",
+            "http://www.router-switch.com/dell-poweredge-t330-xeon-e3-1240-v5-32gb-2tb-server.html",
+            "http://www.itpro.co.uk/server/23980/dell-poweredge-r730-review",
+            "https://www.indiamart.com/proddetail/hp-proliant-dl20-gen-9-server-866233-375-13489239973.html",
+            "https://www.cisco.com/c/en/us/products/servers-unified-computing/ucs-b-series-blade-servers/index.html"};
     private final List<ServerDevice> serverDevices = new ArrayList<>();
     private final ServerDeviceService serverDeviceService;
     private final RackService rackService;
@@ -36,6 +44,7 @@ public class ServerDeviceGenerator extends RackableDeviceGenerator implements Ge
             final ServerDevice serverDevice = new ServerDevice();
             serverDevice.setType(AssetType.SERVER_DEVICE);
             serverDevice.setModel(SERVER_DEVICES[i % SERVER_DEVICES.length]);
+            serverDevice.setImageURL(SERVER_DEVICES_IMG_URLS[i % SERVER_DEVICES.length]);
             serverDevice.setSubType(dataFactory.getItem(ServerDeviceType.values()));
             generate(serverDevice, rackService, generators);
             serverDevices.add(serverDeviceService.findByName(serverDevice.getName()));
